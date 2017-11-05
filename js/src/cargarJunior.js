27fd024132cdcd1c937ce6071805018a6b68aa1c
Diego Alejandro Correa Ramirez <dieguini@MacBook-Pro-de-Diego.local>
2017-11-05 12:19:49 -0500

    Cambios logica

--- js/src/cargarJunior.js
+++ js/src/cargarJunior.js
@@ -3,16 +3,16 @@ function cargarJunior() {
         nombre: "Junior",
         preguntas: [
             {
-                texto: "Traeme el informe de costos de este mes",
+                texto: "Traeme el informe de costos de este mes, cualquiera del piso se lo puede facilitar",
                 repeticiones: 0,
                 opciones: [
-                    {
-                        interaccion: "Dejame buscarlo, pero antes necesito un favor",
-                        aceptacion: "listo, lo que tienes que hacer es muy sencillo",
-                        cancelacion: "Vale, sera en otra ocasion",
-                        fallo: "Bueno, sera en otra ocasion",
-                        probable: "listo, aqui esta",
-                        improbable: "No lo tengo a la mano, que pena"
+                    { /// Opcion para el minijuego de disparos
+                        interaccion: "Te podria ayudar, pero esta en la bodega y tenemos un problema de plagas, me ayudarias con eso?",
+                        aceptacion: "listo, vamos para alla",
+                        cancelacion: "Vale, pero no podre darte el informe",
+                        fallo: "Latima, los murcielagos interrumpen el paso",
+                        probable: "Mira, aqui esta el informe, gracias por todo",
+                        improbable: "No lo tenia yo, que pena, lo siento"
                     },
                     {
                         interaccion: "Dejame buscarlo, pero antes necesito un favor",
