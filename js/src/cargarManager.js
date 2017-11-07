92bbe3a8c21bdbed4bdb828d53574e20d14e79be
rodrodogo <rodrodogo@gmai.com>
2017-11-07 00:20:25 -0500

    textos

--- js/src/cargarManager.js
+++ js/src/cargarManager.js
@@ -10,32 +10,32 @@ function cargarManager() {
                         aceptacion: "Estupendo ",
                         cancelacion: "Qué pena torturar es mi parte favorita ",
                         fallo: "Al que torturare será a ti ",
-                        probable: "listo, aqui esta",
-                        improbable: "No lo tengo a la mano, que pena"
+                        probable: "Gracias es hora de trabajar",
+                        improbable: "Está roto, creo que no se podrá hacer nada"
                     },
                     {
-                        interaccion: "Dejame buscarlo, pero antes necesito un favor",
-                        aceptacion: "listo, lo que tienes que hacer es muy sencillo",
-                        cancelacion: "Vale, sera en otra ocasion",
-                        fallo: "Bueno, sera en otra ocasion",
-                        probable: "listo, aqui esta",
-                        improbable: "No lo tengo a la mano, que pena"
+                        interaccion: "Llegaste al lugar adecuado lo podemos hacer por medio de los cristales, ¿te parece? ",
+                        aceptacion: "Que paguen por sus pecados",
+                        cancelacion: "No habrá misericordia ",
+                        fallo: "¿La lava y las almas en pena son mucho para ti?",
+                        probable: "Es hora de castigar",
+                        improbable: "No los cristales se han dañado y están causando felicidad "
                     },
                     {
-                        interaccion: "Dejame buscarlo, pero antes necesito un favor",
-                        aceptacion: "listo, lo que tienes que hacer es muy sencillo",
-                        cancelacion: "Vale, sera en otra ocasion",
-                        fallo: "Bueno, sera en otra ocasion",
-                        probable: "listo, aqui esta",
-                        improbable: "No lo tengo a la mano, que pena"
+                        interaccion: "Es el lugar indicado podemos publicar las intimidades vergonzosas de los trabajadores ¿estás de acuerdo?",
+                        aceptacion: "listo, pero primero muestra tu valor",
+                        cancelacion: "Podría publicar las tuyas ",
+                        fallo: "Se publicó las del jefe ¡!!",
+                        probable: "Lo conseguimos",
+                        improbable: "Parece que nadie tiene nada que ocultar"
                     },
                     {
-                        interaccion: "Dejame buscarlo, pero antes necesito un favor",
-                        aceptacion: "listo, lo que tienes que hacer es muy sencillo",
-                        cancelacion: "Vale, sera en otra ocasion",
-                        fallo: "Bueno, sera en otra ocasion",
-                        probable: "listo, aqui esta",
-                        improbable: "No lo tengo a la mano, que pena"
+                        interaccion: "Vamos a torturarlos pero primero, ¿me ayudas?",
+                        aceptacion: "Será sencillo ",
+                        cancelacion: "No hay mejor pecado que la cobardía ",
+                        fallo: "Que lastima ahora tendrás que ser mi mascota",
+                        probable: "Es hora de la tortura",
+                        improbable: "Parece que les gusta que los torturen, mmmm que lujuria "
                     }
                 ]
             },
@@ -44,36 +44,36 @@ function cargarManager() {
                 repeticiones: 0,
                 opciones: [
                     {
-                        interaccion: "Dejame buscarlo, pero antes necesito un favor",
-                        aceptacion: "listo, lo que tienes que hacer es muy sencillo",
-                        cancelacion: "Vale, sera en otra ocasion",
-                        fallo: "Bueno, sera en otra ocasion",
-                        probable: "listo, aqui esta",
-                        improbable: "No lo tengo a la mano, que pena"
+                        interaccion: "Que Re hagan todo el trabajo, me gusta, me gusta ¿pero primero buscarías algo que necesito?",
+                        aceptacion: "Estupendo ",
+                        cancelacion: "Qué tenía una estupenda idea",
+                        fallo: "Así no se puede",
+                        probable: "Inesperado pantallazo azul esta listooo",
+                        improbable: "Está roto, creo que no se podrá hacer nada"
                     },
                     {
-                        interaccion: "Dejame buscarlo, pero antes necesito un favor",
-                        aceptacion: "listo, lo que tienes que hacer es muy sencillo",
-                        cancelacion: "Vale, sera en otra ocasion",
-                        fallo: "Bueno, sera en otra ocasion",
-                        probable: "listo, aqui esta",
-                        improbable: "No lo tengo a la mano, que pena"
+                        interaccion: "Llegaste al lugar adecuado lo podemos hacer por medio de los cristales, ¿te parece? ",
+                        aceptacion: "Que paguen por sus pecados",
+                        cancelacion: "No habrá misericordia ",
+                        fallo: "¿La lava y las almas en pena son mucho para ti?",
+                        probable: "Cambio inesperado de variables disponible",
+                        improbable: "No los cristales se han dañado y están causando productividad "
                     },
                     {
-                        interaccion: "Dejame buscarlo, pero antes necesito un favor",
-                        aceptacion: "listo, lo que tienes que hacer es muy sencillo",
-                        cancelacion: "Vale, sera en otra ocasion",
-                        fallo: "Bueno, sera en otra ocasion",
-                        probable: "listo, aqui esta",
-                        improbable: "No lo tengo a la mano, que pena"
+                        interaccion: "Es el lugar indicado podemos publicar el mensaje por fb así se enteraran cuanto tengan que corregir todo ¿estás de acuerdo?",
+                        aceptacion: "listo, pero primero muestra tu valor",
+                        cancelacion: "... ",
+                        fallo: "No eres digno",
+                        probable: "Lo conseguimos",
+                        improbable: "Parece que no vieron el mensaje"
                     },
                     {
-                        interaccion: "Dejame buscarlo, pero antes necesito un favor",
-                        aceptacion: "listo, lo que tienes que hacer es muy sencillo",
-                        cancelacion: "Vale, sera en otra ocasion",
-                        fallo: "Bueno, sera en otra ocasion",
-                        probable: "listo, aqui esta",
-                        improbable: "No lo tengo a la mano, que pena"
+                        interaccion: "Vamos a retrasarlos pero primero, ¿me ayudas?",
+                        aceptacion: "Será sencillo ",
+                        cancelacion: "No hay mejor pecado que la cobardía ",
+                        fallo: "Que lastima ahora tendrás que ser mi mascota",
+                        probable: "Es hora de dar la mala noticia",
+                        improbable: "Son bastantes perezosos, procrastinaron la nueva tarea "
                     }
                 ]
             },
@@ -82,36 +82,36 @@ function cargarManager() {
                 repeticiones: 0,
                 opciones: [
                     {
-                        interaccion: "Dejame buscarlo, pero antes necesito un favor",
-                        aceptacion: "listo, lo que tienes que hacer es muy sencillo",
-                        cancelacion: "Vale, sera en otra ocasion",
-                        fallo: "Bueno, sera en otra ocasion",
-                        probable: "listo, aqui esta",
-                        improbable: "No lo tengo a la mano, que pena"
+                        interaccion: "Que trabajen el doble, me gusta ¿pero primero buscarías algo que necesito?",
+                        aceptacion: "Estupendo ",
+                        cancelacion: "Qué pena torturar es mi parte favorita ",
+                        fallo: "Al que le reduciré el sueldo será a ti ",
+                        probable: "Gracias es hora de trabajar",
+                        improbable: "Está roto, creo que no se podrá hacer nada"
                     },
                     {
-                        interaccion: "Dejame buscarlo, pero antes necesito un favor",
-                        aceptacion: "listo, lo que tienes que hacer es muy sencillo",
-                        cancelacion: "Vale, sera en otra ocasion",
-                        fallo: "Bueno, sera en otra ocasion",
-                        probable: "listo, aqui esta",
-                        improbable: "No lo tengo a la mano, que pena"
+                        interaccion: "Llegaste al lugar adecuado lo podemos hacer por medio de los cristales, ¿te parece? ",
+                        aceptacion: "Que paguen por sus pecados",
+                        cancelacion: "No habrá misericordia ",
+                        fallo: "¿La lava y las almas en pena son mucho para ti?",
+                        probable: "Es hora de castigar",
+                        improbable: "No los cristales se han dañado y están aumentando su salario"
                     },
                     {
-                        interaccion: "Dejame buscarlo, pero antes necesito un favor",
-                        aceptacion: "listo, lo que tienes que hacer es muy sencillo",
-                        cancelacion: "Vale, sera en otra ocasion",
-                        fallo: "Bueno, sera en otra ocasion",
-                        probable: "listo, aqui esta",
-                        improbable: "No lo tengo a la mano, que pena"
+                        interaccion: "Es el lugar indicado podemos distraerlos para que salgan más tarde ¿estás de acuerdo?",
+                        aceptacion: "listo, pero primero muestra tu valor",
+                        cancelacion: "Que mal",
+                        fallo: "oh mira un nuevo juego",
+                        probable: "Lo conseguimos",
+                        improbable: "Parece que nadie todos ignoran tu perfil "
                     },
                     {
-                        interaccion: "Dejame buscarlo, pero antes necesito un favor",
-                        aceptacion: "listo, lo que tienes que hacer es muy sencillo",
-                        cancelacion: "Vale, sera en otra ocasion",
-                        fallo: "Bueno, sera en otra ocasion",
-                        probable: "listo, aqui esta",
-                        improbable: "No lo tengo a la mano, que pena"
+                        interaccion: "Vamos a reducirles el salario pero primero, ¿me ayudas?",
+                        aceptacion: "Será sencillo ",
+                        cancelacion: "No hay mejor pecado que la cobardía ",
+                        fallo: "Que lastima ahora tendrás que ser mi mascota",
+                        probable: "Es hora de la tortura",
+                        improbable: "Parece que les gusta salir tarde, Tal vez no quieren llegar a casa"
                     }
                 ]
             }
